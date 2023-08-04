31acaa6296440f6022fc626aa815dd929533c8ca
Nokse22 <lorenzo.ubaldi22@gmail.com>
2023-08-04 15:19:42 +0200

    added buttons to copy, paste, delete

--- src/window.py
+++ src/window.py
@@ -18,11 +18,13 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from gi.repository import Adw
-from gi.repository import Gtk, Gio
+from gi.repository import Gtk, Gio, Gdk
 
 @Gtk.Template(resource_path='/io/github/nokse22/minitext/window.ui')
 class MiniTextWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'MiniTextWindow'
+
+    text_view = Gtk.Template.Child("text_view")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -35,3 +37,33 @@ class MiniTextWindow(Adw.ApplicationWindow):
         self.settings.bind(
             "window-height", self, "default-height", Gio.SettingsBindFlags.DEFAULT
         )
+
+    @Gtk.Template.Callback("on_copy_action")
+    def on_copy_action(self, *args):
+        print("copy")
+        text_buffer = self.text_view.get_buffer()
+        start = text_buffer.get_start_iter()
+        end = text_buffer.get_end_iter()
+        text = text_buffer.get_text(start, end, False)
+        self.copy_to_clipboard(text)
+
+    @Gtk.Template.Callback("on_paste_action")
+    def on_paste_action(self, *args):
+        clipboard = Gdk.Display().get_default().get_clipboard()
+        def callback(clipboard, res, data):
+            text = clipboard.read_text_finish(res)
+            text_buffer = self.text_view.get_buffer()
+            text_buffer.set_text(text)
+        data = {}
+        res = clipboard.read_text_async(None, callback, data)
+
+    @Gtk.Template.Callback("on_delete_action")
+    def on_delete_action(self, *args):
+        print("delete")
+        text_buffer = self.text_view.get_buffer()
+        text_buffer.set_text("")
+
+    def copy_to_clipboard(self, text):
+        clipboard = Gdk.Display().get_default().get_clipboard()
+        clipboard.set(text)
+        #clipboard.store()
